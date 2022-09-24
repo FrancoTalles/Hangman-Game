101b2d7454662c1c184672cce0816a727d96d168
FrancoTalles <francotallesextreme@gmail.com>
2022-09-24 02:36:35 -0400

    Jogo feito (falta bonus e detalhes)

--- src/App.js
+++ src/App.js
@@ -45,18 +45,38 @@ export default function App() {
   const [erros, setErros] = useState(1);
   const [imagem, setImagem] = useState(estagios[0]);
   const [escolhidas, setEscolhidas] = useState(alfabeto);
-  let [arrayPalavra, setArrayPalavra] = useState([]);
+  const [arrayPalavra, setArrayPalavra] = useState([]);
+  const [arrayPalavraComAcento, setArrayPalavraComAcento] = useState([]);
+  const [tracos, setTracos] = useState([]);
+  const [caractereClass, setCaractereClass] = useState("caractere");
+  const [ButtonClass, setClassButton] = useState("off");
+  const [interruptor, setInterruptor] = useState("down");
+  const [palavraInput, setPalavraInput] = useState("");
+  let auxpalavra = "";
 
   function start() {
-    console.log("apertado");
     setEscolhidas([]);
+    setCaractereClass("caractere");
+    setClassButton("on");
+    setErros(1);
+    setImagem(estagios[0]);
+    setInterruptor("up");
     let escolhida = Math.floor(Math.random() * palavras.length);
     let palavra = palavras[escolhida];
-    console.log(palavra);
+    let palavraSeparadaComAcento = [...palavra];
+    setArrayPalavraComAcento(palavraSeparadaComAcento);
+    let auxTracos = [];
     palavra = palavra.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    setArrayPalavra([...palavra]);
+    let palavraSeparadaSemAcento = [...palavra];
+    setArrayPalavra(palavraSeparadaSemAcento);
     console.log(palavra);
-    console.log(arrayPalavra);
+    console.log(palavraSeparadaComAcento);
+    console.log(palavraSeparadaSemAcento);
+    for (let i = 0; i < palavra.length; i++) {
+      auxTracos.push("_");
+      console.log(auxTracos);
+      setTracos(auxTracos);
+    }
   }
 
   function apertou(letra) {
@@ -65,15 +85,25 @@ export default function App() {
     setEscolhidas(adicionaEscolhidas);
     let i = 0;
     let acertou = 0;
-    while(i < arrayPalavra.length){
-      if(letra === arrayPalavra[i]){
-        console.log("acertou")
-        acertou++
+    let auxTracos = [...tracos];
+    while (i < arrayPalavra.length) {
+      if (letra === arrayPalavra[i]) {
+        console.log("acertou");
+        acertou++;
+        console.log(auxTracos);
+        auxTracos[i] = arrayPalavraComAcento[i];
+        console.log(auxTracos);
       }
-      i++
+      i++;
     }
-    if (acertou <= 0){
+    auxpalavra = auxTracos.join("");
+    setTracos(auxTracos);
+    if (acertou > 0) {
+      final();
+    }
+    if (acertou <= 0) {
       erro();
+      final();
     }
   }
 
@@ -82,6 +112,43 @@ export default function App() {
     setImagem(estagios[erros]);
   }
 
+  function final() {
+    console.log(imagem);
+    console.log(estagios[erros]);
+    console.log(img6);
+    console.log(tracos);
+    console.log(arrayPalavraComAcento.join(""));
+    console.log(tracos.join(""));
+    console.log(auxpalavra);
+
+    if (estagios[erros] === img6) {
+      setCaractereClass("errou");
+      setEscolhidas(alfabeto);
+      setTracos(arrayPalavraComAcento);
+      setClassButton("off");
+    }
+    if (arrayPalavraComAcento.join("") === auxpalavra) {
+      setCaractereClass("acertou");
+      setEscolhidas(alfabeto);
+      setClassButton("off");
+    }
+  }
+
+  function finalInput() {
+    if (palavraInput.toLowerCase() === arrayPalavra.join("")) {
+      setTracos(arrayPalavraComAcento);
+      setClassButton("off");
+      setEscolhidas(alfabeto);
+      setCaractereClass("acertou");
+    } else {
+      setTracos(arrayPalavraComAcento);
+      setClassButton("off");
+      setEscolhidas(alfabeto);
+      setCaractereClass("errou");
+      setImagem(estagios[6]);
+    }
+    setPalavraInput("");
+  }
 
   return (
     <>
@@ -91,9 +158,9 @@ export default function App() {
           Escolher Palavra
         </button>
         <ul className="resposta">
-          {arrayPalavra.map((l, index) => (
-            <li key={index} className="caractere">
-              _
+          {tracos.map((l, index) => (
+            <li key={index} className={caractereClass}>
+              {l}
             </li>
           ))}
         </ul>
@@ -102,14 +169,33 @@ export default function App() {
         {alfabeto.map((letra, index) => (
           <div
             key={index}
-            className={escolhidas.includes(letra) ? "desabilitado" : "habilitado"}
+            className={
+              escolhidas.includes(letra) ? "desabilitado" : "habilitado"
+            }
             onClick={() => apertou(letra)}
           >
             {letra}
           </div>
         ))}
       </div>
-      <div className="input"></div>
+      <div className="input">
+        <p>Já sei a palavra!</p>
+        <input
+          value={palavraInput}
+          onChange={(event) =>
+            setPalavraInput(
+              event.target.value
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+            )
+          }
+          className={interruptor}
+          placeholder="Qual é seu chute?"
+        ></input>
+        <button onClick={finalInput} className={ButtonClass}>
+          Chutar
+        </button>
+      </div>
     </>
   );
 }
